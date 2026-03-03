b65f0a2cff3b0e3fedd1cde1bc526400a470aaef
Lydia Roan <magicdragon137@outlook.com>
2026-03-02 23:24:58 -0600

    I'M A GENUIS

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.alert("Incoming message");
+
 document.querySelectorAll('.item').forEach((el, i) => {
     el.style.animationDelay = (i * 5) + 's';
 });
+
+setTimeout(() => {
+    window.location.href = "index.html";
+}, 30000);
